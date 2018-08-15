0b4444c3ef294eddd8db5c1969d773670a60b4a9
HumanoidSandvichDispenser <troll1.lolzz@gmail.com>
2018-08-14 23:24:51 -0700

    added 2 diff panels to test

--- sandvichcooking/lua/entities/food_bagel/cl_init.lua
+++ sandvichcooking/lua/entities/food_bagel/cl_init.lua
@@ -9,7 +9,7 @@ function ENT:DrawTranslucent()
     local x, y, z = self:GetAngles().x, LocalPlayer():EyeAngles().Yaw - 90, 90
 
     // Sets position of 3D2D with a position and angle offset
-    cam.Start3D2D(Vector(self:GetPos().x, self:GetPos().y, self:GetPos().z + 16), Angle(0, y, z), 0.1)
+    cam.Start3D2D(Vector(self:GetPos().x, self:GetPos().y, self:GetPos().z + 16), Angle(0, y, z), 0.02)
         draw.RoundedBox(0, -375, 0, 750, 225, Color(0, 0, 0, 155))
         draw.SimpleText(self.ItemName, "3D2DTitle", 0, 10, Color( 255, 255, 255, 255 ),  TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP) // 10 for ones with rarity of 1, otherwise 0
         draw.SimpleText("+" .. self.Quality .. " HP", "3D2DLabel", 0, 140, Color( 255, 255, 255, 255 ),  TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
--- sandvichcooking/lua/entities/food_bonk_blutonium_berry/cl_init.lua
+++ sandvichcooking/lua/entities/food_bonk_blutonium_berry/cl_init.lua
@@ -1,23 +1,6 @@
 include("shared.lua")
 include("init.lua")
 
-surface.CreateFont("Default", {
-    font = "Arial",
-    size = 13,
-    weight = 400,
-    blursize = 0,
-    scanlines = 0,
-    antialias = true,
-    underline = false,
-    italic = false,
-    strikeout = false,
-    symbol = false,
-    rotary = false,
-    shadow = false,
-    additive = false,
-    outline = true,
-})
-
 function ENT:Draw()
     self:DrawModel()
 end
@@ -26,10 +9,10 @@ function ENT:DrawTranslucent()
     local x, y, z = self:GetAngles().x, LocalPlayer():EyeAngles().Yaw - 90, 90
 
     // Sets position of 3D2D with a position and angle offset
-    cam.Start3D2D(Vector(self:GetPos().x, self:GetPos().y, self:GetPos().z + 16), Angle(0, y, z), 0.1)
-        draw.RoundedBox(0, -135, 0, 270, 52, Color(0, 0, 0, 155))
-        draw.RoundedBox(0, -135, 0, 270, 32, Color(244, 68, 224, 225))
-        draw.SimpleText(self.ItemName, "DermaLarge", 0, 0, Color( 255, 255, 255, 255 ),  TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
-        draw.SimpleText("+" .. self.Quality .. " HP", "DermaDefaultBold", 0, 35, Color( 255, 255, 255, 255 ),  TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    cam.Start3D2D(Vector(self:GetPos().x, self:GetPos().y, self:GetPos().z + 16), Angle(0, y, z), 0.02)
+        draw.RoundedBox(0, -675, 0, 1350, 225, Color(0, 0, 0, 155))
+        draw.RoundedBox(0, -675, 32, 1350, 4, Color(244, 68, 224, 225))
+        draw.SimpleText(self.ItemName, "3D2DTitle", 0, 0, Color( 255, 255, 255, 255 ),  TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+        draw.SimpleText("+" .. self.Quality .. " HP", "3D2DLabel", 0, 140, Color( 255, 255, 255, 255 ),  TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
     cam.End3D2D()
 end
